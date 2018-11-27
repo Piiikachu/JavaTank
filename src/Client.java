39d3ec64c985dd06ee9b59b31cf4e5c458980aba
Piiikachu <314475440@qq.com>
2018-11-27 22:54:27 +0800

    gui

--- src/Client.java
+++ src/Client.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.Ellipse2D;
 
 
 public class Client extends JFrame {
@@ -7,9 +9,14 @@ public class Client extends JFrame {
     private int height;
 
     public Client(String title,int width,int height){
+        super(title);
         this.width=width;
         this.height=height;
-        this.setTitle(title);
+
+        MyPanel panel=new MyPanel();
+        panel.setPreferredSize(new Dimension(width,height));
+        setContentPane(panel);
+        pack();
         this.setVisible(true);
         this.setSize(width, height);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -22,8 +29,30 @@ public class Client extends JFrame {
         return width;
     }
 
-
     public int getFrameHeight() {
         return height;
     }
+
+    private class MyPanel extends JPanel{
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+
+            Graphics2D g2d=(Graphics2D)g;
+
+            VisHelper.setStrokeWidth(g2d,5);
+
+            g2d.setColor(Color.cyan);
+            VisHelper.strokeCircle(g2d,width/2,height/2,200);
+
+            g2d.setColor(Color.ORANGE);
+            VisHelper.fillCircle(g2d,width/2,height/2,200);
+        }
+
+        @Override
+        public Dimension getPreferredSize() {
+            return new Dimension(width,height);
+        }
+    }
+
 }
